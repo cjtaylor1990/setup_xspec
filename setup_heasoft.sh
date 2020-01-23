ee092aba682a37ff3a6350c6ad4310eac3d0263f
Corbin Taylor <cjtaylor@astro.umd.edu>
2020-01-22 22:35:28 -0500

    Changed the variable name for the path to the original location of the user in setup_heasoft.sh.

--- setup_heasoft.sh
+++ setup_heasoft.sh
@@ -5,7 +5,7 @@ heasoftVersion=$1 #Heasoft verison to set up
 destination=$2 #Relative (or absolute) path directory where to place the HEASoft directory
 
 #Finding absolute path to current location to return user after completed
-currentLocation=$(pwd)
+originalLocation=$(pwd)
 
 #HEASoft tar.gz (gzip tarball)
 heasoftFile="heasoft-6.26.1src.tar.gz"
@@ -43,4 +43,4 @@ echo "SETENV ${absolutePathDestination}/heasoft-${heasoftVersion}/" >> .cshrc
 alias heainit "source $HEADAS/headas-init.csh"
 
 #Returning user
-cd ${currentLocation}
+cd ${originalLocation}
